5437fc2b804f9505282407c26e5e0e4d49380c52
johustle-repo <johustle-repo@users.noreply.github.com>
2026-09-01 17:08:32 +0800

    Apply LeadGen Central browser branding

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,16 +30,14 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/leadgen-icon.svg" type="image/svg+xml">
 
         @fonts
 
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         <x-inertia::head>
-            <title>{{ config('app.name', 'Laravel') }}</title>
+            <title>LeadGen Central</title>
         </x-inertia::head>
     </head>
     <body class="font-sans antialiased">
